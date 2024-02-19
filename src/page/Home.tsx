3062e15d53c71502980bedc808bf15a20142443f
tom@pinnacleprocurementllc.com <tom@pinnacleprocurementllc.com>
2024-02-19 17:48:07 -0600

    Added logo

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,6 +1,7 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import logo from '../assets/logo.png';
 
 const user = {
   name: 'Tom Cook',
@@ -44,12 +45,12 @@ export default function Example() {
                     <div className="flex flex-shrink-0 items-center">
                       <img
                         className="block h-8 w-auto lg:hidden"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                        src={logo}
                         alt="Your Company"
                       />
                       <img
                         className="hidden h-8 w-auto lg:block"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                        src={logo}
                         alt="Your Company"
                       />
                     </div>
