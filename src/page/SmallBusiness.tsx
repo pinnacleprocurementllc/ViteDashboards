6f248563de4e9ef128321462d7867e9a9ce913d9
tom@pinnacleprocurementllc.com <tom@pinnacleprocurementllc.com>
2024-02-19 18:52:38 -0600

    Removed recipientCard from recipient

--- src/page/SmallBusiness.tsx
+++ src/page/SmallBusiness.tsx
@@ -27,21 +27,65 @@ const SmallBusiness = () => {
         fetchSmallBusiness();
     }, []);
     return (
-        <div>
-            <h1>Top Receipients for Contract work in Missouri</h1>
+        <div className="px-4 sm:px-6 lg:px-8">
+        <div className="sm:flex sm:items-center">
+            <div className="sm:flex-auto">
+            <h1 className="text-base font-semibold leading-6 text-gray-900">Small Business Contracts</h1>
+            <p className="mt-2 text-sm text-gray-700">
+                A list of contracts...
+            </p>
+            </div>
+        </div>
+        <div className="mt-8 flow-root">
+            <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                <table className="min-w-full divide-y divide-gray-300">
+                <thead>
+                    <tr>
+                    <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3">
+                        Recipient Name
+                    </th>
+                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                        Award Amount
+                    </th>
+                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                        Place of Performance State Code
+                    </th>
+                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                        Award ID
+                    </th>
+                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                        Awarding Sub Agency
+                    </th>
+                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                        Start Date
+                    </th>
+                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                        End Date
+                    </th>
 
-            {smallbusinesses.map((smallbusiness, index) => (
-                <li key={index}>
-                    <p>Recipient Name: {smallbusiness["Recipient Name"]}</p>
-                    <p>Award Amount: {smallbusiness["Award Amount"]}</p>
-                    <p>Place of Performance: {smallbusiness["Place of Performance State Code"]}</p>
-                    <p>Award Id: {smallbusiness["Award ID"]}</p>
-                    <p>Award Agency: {smallbusiness["Awarding Agency"]}</p>
-                    <p>Award Sub-Agency: {smallbusiness["Awarding Sub Agency"]}</p>
-                    <p>Start Date: {smallbusiness["Start Date"]}</p>
-                    <p>End Date: {smallbusiness["End Date"]}</p>
-                </li>
-            ))}
+                    </tr>
+                </thead>
+                <tbody className="bg-white">
+                    {smallbusinesses.map((smallbusiness, index) => (
+                    <tr key={index} className="even:bg-gray-50">
+                        <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">
+                        {smallbusiness["Recipient Name"]}
+                        </td>
+                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{smallbusiness["Award Amount"]}</td>
+                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{smallbusiness["Place of Performance State Code"]}</td>
+                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{smallbusiness["Award ID"]}</td>
+                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{smallbusiness["Awarding Sub Agency"]}</td>
+                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{smallbusiness["Start Date"]}</td>
+                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{smallbusiness["End Date"]}</td>
+
+                    </tr>
+                    ))}
+                </tbody>
+                </table>
+            </div>
+            </div>
+        </div>
         </div>
     )
 
